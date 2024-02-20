f562e0dce75f5a48b84787ae22fe18767d6524fd
Thomas Bernard <thomas.bernard.310@gmail.com>
2024-02-20 11:41:36 +0100

    add modal drawer

--- app/src/main/java/fr/group5/magellangpt/presentation/main/MainScreen.kt
+++ app/src/main/java/fr/group5/magellangpt/presentation/main/MainScreen.kt
@@ -1,5 +1,6 @@
 package fr.group5.magellangpt.presentation.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,47 +9,107 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import fr.group5.magellangpt.R
 import fr.thomasbernard03.composents.TextField
 import fr.thomasbernard03.composents.buttons.SquaredButton
+import kotlinx.coroutines.launch
 
 @Composable
-fun MainScreen(uiState: MainUiState, onEvent: (MainEvent) -> Unit){
-    Column(
-        modifier = Modifier
-            .background(Color.LightGray)
-            .fillMaxSize()
-    ) {
+fun MainScreen(
+    uiState: MainUiState,
+    onEvent: (MainEvent) -> Unit
+){
 
-        Spacer(modifier = Modifier.weight(1f))
-        
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-        Row(
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text("Drawer title", modifier = Modifier.padding(16.dp))
+                Divider()
+            }
+        }) {
+        Column(
             modifier = Modifier
-                .background(
-                    color = MaterialTheme.colorScheme.background,
-                    shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp)
-                )
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(6.dp)
+                .background(Color.LightGray)
+                .fillMaxSize()
         ) {
-            TextField(
-                placeholder = stringResource(id = R.string.message),
-                modifier = Modifier.weight(1f),
-                text = uiState.query,
-                onTextChange = { onEvent(MainEvent.OnQueryChanged(it)) })
-            
-            SquaredButton(resource = R.drawable.share, onClick = {  })
-        }
+            Row(
+                modifier = Modifier.padding(12.dp)
+            ) {
+                SquaredButton(
+                    resource = R.drawable.vectormenu,
+                    onClick = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    })
+            }
 
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Image(
+                    modifier = Modifier.size(200.dp),
+                    contentScale = ContentScale.FillWidth,
+                    painter = painterResource(id = R.drawable.file),
+                    contentDescription = "file")
+
+                Text(
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.empty_conversation_placeholder),
+                    style = MaterialTheme.typography.titleSmall)
+            }
+
+
+            Row(
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.background,
+                        shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp)
+                    )
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                TextField(
+                    placeholder = stringResource(id = R.string.message),
+                    modifier = Modifier.weight(1f),
+                    text = uiState.query,
+                    onTextChange = { onEvent(MainEvent.OnQueryChanged(it)) })
+
+                SquaredButton(resource = R.drawable.share, onClick = {  })
+            }
+
+        }
     }
 }
