38e86286210473f7a024295ee14613d414c2a081
Thomas Bernard <thomas.bernard.310@gmail.com>
2024-03-11 09:40:27 +0100

    add comments

--- app/src/main/java/fr/group5/magellangpt/domain/repositories/AuthenticationRepository.kt
+++ app/src/main/java/fr/group5/magellangpt/domain/repositories/AuthenticationRepository.kt
@@ -4,9 +4,23 @@ import android.app.Activity
 
 interface AuthenticationRepository {
 
+    /**
+     * Login the user
+     * @param activity the activity (needed for the MSAL library)
+     * @param onSuccess the success callback (the user is logged in)
+     * @param onCancel the cancel callback (the user cancelled the login)
+     * @param onError the error callback (an error occurred)
+     */
     fun login(activity : Activity, onSuccess : () -> Unit, onCancel : () -> Unit, onError : () -> Unit)
 
+    /**
+     * Logout the user, clear the credentials
+     */
     suspend fun logout()
 
+    /**
+     * Check if the user is connected
+     * @param onResult the result callback
+     */
     suspend fun userConnected(onResult : (Boolean) -> Unit)
 }
